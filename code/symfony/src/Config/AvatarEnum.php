8ddb3b17d775548a7b80b933e2483ea3a3a0c0a4
AC Développement <freelance@alexandrechauvin.fr>
2025-03-08 02:24:10 +0100

    Add: new avatars

--- code/symfony/src/Config/AvatarEnum.php
+++ code/symfony/src/Config/AvatarEnum.php
@@ -9,13 +9,25 @@ enum AvatarEnum: string
     case alpif    = "/alpif.jpg";
     case annelise = "/annelise.jpg";
     case arthur   = "/arthur.jpg";
+    case cam      = "/cam.jpg";
+    case cat      = "/cat.jpg";
     case clelia   = "/clelia.jpg";
+    case erwan    = "/erwan.jpg";
+    case fab      = "/fab.jpg";
+    case gui      = "/gui.jpg";
     case jo       = "/jo.jpg";
     case joris    = "/joris.jpg";
     case leo      = "/leo.jpg";
+    case lison    = "/lison.jpg";
     case lucas    = "/lucas.jpg";
+    case lulu     = "/lulu.jpg";
     case maff     = "/maff.jpg";
+    case mama     = "/mama.jpg";
     case manon    = "/manon.jpg";
+    case manu     = "/manu.jpg";
+    case mayo     = "/mayo.jpg";
+    case mich     = "/mich.jpg";
+    case pat      = "/pat.jpg";
     case pogs     = "/pogs.jpg";
     case tristan  = "/tristan.jpg";
 
